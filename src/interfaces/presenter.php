e3caafb8fb73af7c3ad099ccdfac4d9638d17c85
technote <technote.space@gmail.com>
2019-03-15 12:35:08 +0900

    v0.0.16

--- src/interfaces/presenter.php
+++ src/interfaces/presenter.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Presenter Interfaces Presenter
  *
- * @version 0.0.11
+ * @version 0.0.16
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
